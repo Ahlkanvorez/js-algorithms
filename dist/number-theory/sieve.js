a53871356d9055958c10b0a08ed49bf21d1c2b02
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-27 11:34:33 -0500

    Added basic comments to the eratosthenes sieve

--- dist/number-theory/sieve.js
+++ dist/number-theory/sieve.js
@@ -3,9 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+// Returns a generator that sequentially generates every prime from least to greatest.
 const eratosthenes = function* () {
+    // Start with the first prime.
     const primes = [2];
-    yield 2;
+    yield primes[0];
     for (let i = primes[primes.length - 1];; ++i) {
         if (primes.every(p => i % p !== 0)) {
             primes[primes.length] = i;
--- src/number-theory/sieve.js
+++ src/number-theory/sieve.js
@@ -1,8 +1,15 @@
+// Returns a generator that sequentially generates every prime.
 const eratosthenes = function *() {
+    // Start with the first prime.
     const primes = [ 2 ];
     yield 2;
+    
+    // Check every successive number for primailty.
     for (let i = primes[primes.length - 1]; ; ++i) {
+        // Since every prime less than i has already been found,
+        // i is prime if and only if none of the primes found thusfar divide it.
         if (primes.every(p => i % p !== 0)) {
+            // Record the newfound prime for use in generating the next prime.
             primes[primes.length] = i;
             yield i;
         }
